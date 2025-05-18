6465dd992623472bdb5a087ad00cdcc18ac5da6d
juniorhernandez <81132842+juniorhernandez@users.noreply.github.com>
2025-05-17 22:17:05 -0600

    se limpia codigo

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,61 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-import Keycloak from 'keycloak-js'
+import Keycloak from "keycloak-js";
 
-const app = createApp(App)
+const app = createApp(App);
 
 // Desactivar Vue Devtools en producción
 if (import.meta.env.PROD) {
-  (window as any).__VUE_DEVTOOLS_GLOBAL_HOOK__ = undefined
+  (window as any).__VUE_DEVTOOLS_GLOBAL_HOOK__ = undefined;
 }
 
-app.use(createPinia())
-app.use(router)
+// Instancias y plugins
+app.use(createPinia());
+app.use(router);
 
+// Instancia de Keycloak
 const keycloak = new Keycloak({
   url: import.meta.env.VITE_KEYCLOAK_URL,
   realm: import.meta.env.VITE_KEYCLOAK_REALM,
-  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID
-})
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+});
 
-;(window as any).$keycloak = keycloak
+
+(window as any).$keycloak = keycloak;
 
 keycloak
-  .init({ onLoad: 'login-required' })
+  .init({
+    onLoad: "login-required",
+    checkLoginIframe: false,
+    flow: "standard",
+    pkceMethod: "S256",
+    scope: "openid",
+  })
   .then((authenticated) => {
     if (authenticated) {
-      app.mount('#app')
+      app.mount("#app");
+
+      if (
+        window.location.hash.includes("state") ||
+        window.location.href.includes("code=")
+      ) {
+        window.history.replaceState(
+          {},
+          document.title,
+          window.location.pathname
+        );
+      }
     } else {
-      console.warn('No se pudo autenticar')
-      window.location.reload()
+      console.warn("No se pudo autenticar");
+      window.location.reload();
     }
   })
   .catch((err) => {
-    console.error('Error de autenticación', err)
-    window.location.reload()
-  })
+    console.error("Error de autenticación", err);
+    window.location.reload();
+  });
